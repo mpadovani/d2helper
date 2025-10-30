479264c5d8d744202919c21c0f9aa5e3e640bbcd
Mateus Padovani <mateuspadovani@gmail.com>
2025-10-30 18:07:22 -0300

    remove not called method

--- src/main/java/com/java/d2helper/infrastructure/repository/TerrorzoneRepository.java
+++ src/main/java/com/java/d2helper/infrastructure/repository/TerrorzoneRepository.java
@@ -3,6 +3,5 @@ package com.java.d2helper.infrastructure.repository;
 import com.java.d2helper.domain.model.Terrorzone;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface TerrorzoneRepository extends MongoRepository<Terrorzone,Long> {
-    Terrorzone findTopByOrderByCreatedAtDesc();
-}
+public interface TerrorzoneRepository extends MongoRepository<Terrorzone,Long> { }
+g
